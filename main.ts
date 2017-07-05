18954593ebd8cdcdf32af824ae3d8f5e83563615
徐培堯 <st89382000@gmail.com>
2017-07-05 10:06:22 +0800

    基本功能完成

---
+++ main.ts
@@ -0,0 +1,42 @@
+interface UserInfo{
+    user:string,
+    password:string
+}
+declare var chrome:any,Cookies:any;
+function loadUserInfo(){
+    var id = Cookies.get('userId');
+    var password = Cookies.get('userPassword');
+    if(!id)return null;
+    return {
+        user: id,
+        password: password
+    };
+}
+function saveUserInfo(userInfo:UserInfo){
+     Cookies.set('userId',userInfo.user , { expires: 365 * 10 });
+     Cookies.set('userPassword',userInfo.password , { expires: 365 * 10 });
+}
+
+(async()=>{    
+    var userInput = <HTMLInputElement>document.getElementsByName("user")[0];
+    var passwordInput = <HTMLInputElement>document.getElementsByName("password")[0];
+    var form = <HTMLFormElement>document.getElementsByTagName("Form")[0];
+    if(!userInput || !passwordInput)return;
+
+    var error = document.getElementById("errorbox");
+    var userInfo = await loadUserInfo();
+    if(!userInfo || error){//init
+        userInfo = {user:null,password:null};
+        userInfo.user = window.prompt("請輸入帳號");
+        userInfo.password = window.prompt("請輸入密碼");
+
+        saveUserInfo(userInfo);
+    }
+    
+    userInput.value = userInfo.user;
+    passwordInput.value = userInfo.password;
+    
+    if(error)return;
+
+    form.submit();
+})();
